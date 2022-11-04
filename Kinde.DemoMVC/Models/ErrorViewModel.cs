91cc8a78ed432acc147120f5106a708d103ad8e8
Андрей Камашев <andkamashev@gmail.com>
2022-11-04 13:56:22 +0400

    remove demo

--- Kinde.DemoMVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Kinde.DemoMVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
