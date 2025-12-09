8c770c1aaf25f75d5bf34c55e23c77b4274886dd
asher.muneer <asher.muneer@whiteboxtech.net>
2025-12-09 13:37:57 +0500

    refactor: rename and realign constructor parameters in PKCES256Configuration

--- Kinde.Api/Models/Configuration/PKCES256Configuration.cs
+++ Kinde.Api/Models/Configuration/PKCES256Configuration.cs
@@ -10,7 +10,7 @@ namespace Kinde.Api.Models.Configuration
             CodeVerifier = new SHA256CodeVerifier();
         }
 
-        public PKCES256Configuration(string clientId, string scope, string clientSecret, string? state, string audience) : base(clientId, scope, clientSecret, state, audience)
+        public PKCES256Configuration(string clientId, string clientSecret, string redirectUri, string scope, string? state) : base(clientId, clientSecret, redirectUri, scope, state)
         {
         }
 
